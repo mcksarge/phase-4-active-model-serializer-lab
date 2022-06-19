2e954f123de0fcf06255d320a43ddcd6839a6e0c
Fred Higgins <higgins.fred.s@gmail.com>
2022-06-18 18:34:37 -0700

    commit tree 53b10a8be6ac5a41112512e7e76237ddd16ce9dd

---
+++ app/serializers/author_post_serializer.rb
@@ -0,0 +1,13 @@
+class AuthorPostSerializer < ActiveModel::Serializer
+  attributes :title, :short_content
+
+  def short_content
+    "#{self.object.title}"
+    "#{self.object.content[0..39]}..."
+  end
+
+  belongs_to :author
+  has_many :tags
+
+
+end
